607bd63a442a13f68d73494052eff56ca6a72ecf
Alex Nguyen <alexvn.edu@gmail.com>
2020-12-15 15:37:51 -0500

    update server

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import flask
-from flask import current_app, request, jsonify
+from flask import current_app, request, jsonify, Response, stream_with_context
 
 from algorithms.kmean.kmean import KMeanClusterer
 from utils.preprocessor import Preprocessor
@@ -14,9 +14,19 @@ def home():
 
 @app.route('/kmean', methods=['POST'])
 def kmean():
-    
     numpy_data = Preprocessor.get_2d_data(request)
-    KMeanClusterer(numpy_data)
-    return "Ler Lew"
+    kmean = KMeanClusterer(numpy_data)
+    print(kmean)
+    print(kmean.wcss())
+    gen = kmean.stream()
+    # # return gen
+    # def generate ():
+    #     import time
+    #     import numpy as np
+    #     for i in range(100):
+    #         li = ['hello', 'how', 'are', 'you']
+    #         yield li[int(np.random.randint(0,3))]
+    # return Response(stream_with_context(generate()))
+    return gen
 
 app.run(debug=True, port=5000)
